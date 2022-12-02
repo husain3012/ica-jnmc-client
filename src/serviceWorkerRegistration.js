cfcd3efea13506b6352182dcd4302c0d9d8a0361
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-12-02 14:50:25 +0530

    servicer worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -24,11 +24,11 @@ const isLocalhost =
 
 export function register(config) {
   // clear cache
-  if (typeof window !== "undefined") {
-    caches.keys().then(function (names) {
-      for (let name of names) caches.delete(name);
-    });
-  }
+  // if (typeof window !== "undefined") {
+  //   caches.keys().then(function (names) {
+  //     for (let name of names) caches.delete(name);
+  //   });
+  // }
 
   if (
     typeof window !== "undefined" &&
@@ -45,9 +45,9 @@ export function register(config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `/service-worker.js`;
 
-      if (isLocalhost) {
+      if ( isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
